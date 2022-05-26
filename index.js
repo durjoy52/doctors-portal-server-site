12d3e0efbfcdee2ad697bf1a7fd606e54c679948
durjoy sarkar <durjoywebdeveloper777@gmail.com>
2022-05-26 12:31:18 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,149 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const  jwt = require('jsonwebtoken');
+const { is } = require("express/lib/request");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.arpk1.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+function verifyJWT(req,res,next){
+  const authHeader = req.headers.authorization
+  if(!authHeader){
+    return res.status(401).send({message:'UnAuthorized access'})
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+    if(err){
+      return res.status(403).send({message:'Forbidden access'})
+    }
+    req.decoded = decoded
+    next()
+  })
+}
+
+async function run() {
+  try {
+      await client.connect()
+      const servicesCollection = client.db('doctors_portal').collection('services')
+      const bookingCollection = client.db('doctors_portal').collection('bookings');
+      const userCollection = client.db('doctors_portal').collection('users');
+      app.get('/service',async(req,res)=>{
+          const cursor = servicesCollection.find()
+          const services = await cursor.toArray()
+          res.send(services)
+      })
+
+      app.get('/admin/:email',async(req,res)=>{
+        const email = req.params.email;
+        const user = await userCollection.findOne({email:email})
+        const isAdmin = user.role === 'admin';
+        res.send({admin:isAdmin})
+      })
+      app.put('/user/admin/:email',verifyJWT, async (req, res) => {
+        const email = req.params.email;
+        const requester = req.decoded.email;
+        const requesterAccount = await userCollection.findOne({email:requester})
+        if(requesterAccount.role === 'admin'){
+          const filter = { email: email };
+          const updateDoc = {
+            $set: {role:'admin'},
+          };
+          const result = await userCollection.updateOne(filter, updateDoc);
+          res.send(result);
+        }else{
+          res.status(403).send({message:'forbidden'})
+        }
+      })
+      app.put('/user/:email', async (req, res) => {
+        const email = req.params.email;
+        const user = req.body;
+        const filter = { email: email };
+        const options = { upsert: true };
+        const updateDoc = {
+          $set: user,
+        };
+        const result = await userCollection.updateOne(filter, updateDoc, options);
+        
+      const token = jwt.sign({email:email},process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+        res.send({ result, token });
+      })
+  
+      app.get('/user', verifyJWT, async (req, res) => {
+        const users = await userCollection.find().toArray();
+        res.send(users);
+      })
+      app.post('/booking', async (req, res) => {
+        const booking = req.body;
+        const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
+        const exists = await bookingCollection.findOne(query);
+        if (exists) {
+          return res.send({ success: false, booking: exists })
+        }
+        const result = await bookingCollection.insertOne(booking);
+        return res.send({ success: true, result });
+      })
+
+       // Warning: This is not the proper way to query multiple collection. 
+    // After learning more about mongodb. use aggregate, lookup, pipeline, match, group
+    app.get('/available', async(req, res) =>{
+      const date = req.query.date;
+
+      // step 1:  get all services
+      const services = await servicesCollection.find().toArray();
+
+      // step 2: get the booking of that day. output: [{}, {}, {}, {}, {}, {}]
+      const query = {date: date};
+      const bookings = await bookingCollection.find(query).toArray();
+
+      // step 3: for each service
+      services.forEach(service=>{
+        // step 4: find bookings for that service. output: [{}, {}, {}, {}]
+        const serviceBookings = bookings.filter(book => book.treatment === service.name);
+        // step 5: select slots for the service Bookings: ['', '', '', '']
+        const bookedSlots = serviceBookings.map(book => book.slot);
+        // step 6: select those slots that are not in bookedSlots
+        const available = service.slots.filter(slot => !bookedSlots.includes(slot));
+        //step 7: set available to slots to make it easier 
+        service.slots = available;
+      });
+     
+
+      res.send(services);
+    })
+
+
+    app.get('/booking',verifyJWT, async(req, res) =>{
+      const patient = req.query.patient;
+      const decodedEmail = req.decoded.email
+      if(patient === decodedEmail){
+        const query = {patient: patient};
+        const bookings = await bookingCollection.find(query).toArray();
+       return res.send(bookings);
+      }else{
+       return req.status(403).send({message:'forbidden access'})
+      }
+    })
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("all is well");
+});
+
+app.listen(port, () => {
+  console.log("your port is", port);
+});
