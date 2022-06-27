7f1b7078685b178720ce179b197f55ac3f27ccf2
durjoy sarkar <durjoywebdeveloper777@gmail.com>
2022-06-28 02:46:32 +0600

    cors problem2

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const  jwt = require('jsonwebtoken');
 const { is } = require("express/lib/request");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
-require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 // middleware
